66efa8a7820a482bbdc9698082c05c9f0b6adbf0
newtonkiragu <karanunewton4@gmail.com>
2018-08-16 20:59:56 +0300

    defined first view function for links page

--- first/views.py
+++ first/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     def get(self, request, **kwargs ):
         return render (request, 'index.html', context=None)
+
+class LinksPageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'links.html', context=None)
