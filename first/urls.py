2e2aef572ec0be22cd11c3776b325f6881a76b90
newtonkiragu <karanunewton4@gmail.com>
2018-08-16 23:30:05 +0300

    updated urls for landing page and links page to use django 2.0 format

--- first/urls.py
+++ first/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from first import views
 
 urlpatterns = [
-    path(r'^$', views.HomePageView.as_view()),
-    path(r'^links/$', views.LinksPageView.as_view())
+    path(r'', views.HomePageView.as_view()),
+    path(r'links/', views.LinksPageView.as_view())
 ]
